e9a780a7dab2c2d336ef09cb27c59a00b13aaff6
itsSuvarna <117782206+itsSuvarna@users.noreply.github.com>
2023-04-06 11:56:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ImageReflector from './components/image-reflector/imageReflector';
+
+function App() {
+  return (
+    <div className="App">
+      <ImageReflector/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/button/button.js
@@ -0,0 +1,11 @@
+import './button.css'
+
+
+export default function Button ({onClickAction,buttonText}) {
+
+    return(
+        <button className = 'getNewImageBtn'onClick={onClickAction} >
+            {buttonText || 'Submit'}
+        </button>
+    )
+}
